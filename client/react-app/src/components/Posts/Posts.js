65c9d6f24c3da59b73f5d23099ac6b0072e9d573
prithivirajmurugan <prithivirajmurugan1996@gmail.com>
2021-07-21 19:49:55 +0530

    implement cards

--- client/react-app/src/components/Posts/Posts.js
+++ client/react-app/src/components/Posts/Posts.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useSelector} from 'react-redux';
+import {Grid,CircularProgress} from '@material-ui/core';
 import Post from './Post/Post';
 import useStyles from './styles';
 
@@ -9,11 +10,23 @@ const Posts = () => {
     });
     console.log(posts);
     const classes = useStyles();
-    return <>
-        <h1 className={classes.h1}>Posts</h1>
-        <Post />
-        <Post />
-        </>
+    return (
+        !posts.length ? <CircularProgress /> : ( 
+            <Grid className={classes.container}
+            container
+            alignItems="strech"
+            spacing={3}>
+             {posts.map((post)=>(
+                 <Grid key={post._id} 
+                 item
+                 xs={12}
+                 sm={6}>
+                     <Post post={post}/>
+                 </Grid>
+             ))}
+            </Grid>
+        )
+    );
 }
 
 export default Posts;
